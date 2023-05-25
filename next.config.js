811716a83dfece60a34c5bc7da40d371226a6df0
hemen47 <hemenbadri@gmail.com>
2023-05-25 12:24:52 +0430

    updated package.json and next configs proxy

--- next.config.js
+++ next.config.js
@@ -2,8 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost']
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
