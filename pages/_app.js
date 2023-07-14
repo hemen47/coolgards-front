935b6e677d5f5fd591a313aa2810f5c620eed271
hemen47 <hemenbadri@gmail.com>
2023-07-14 19:06:59 +0430

    another fixed bug in google analytics code

--- pages/_app.js
+++ pages/_app.js
@@ -114,14 +114,14 @@ function MyApp({ Component, pageProps }) {
               )}
               {renderFooter()}
 
-              <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+              <Script async src={`https://www.googletagmanager.com/gtag/js?id="${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}"`} />
               <Script id="google-analytics">
                 {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID});
+          gtag('config', "${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}");
         `}
               </Script>
             </UserContext.Provider>
